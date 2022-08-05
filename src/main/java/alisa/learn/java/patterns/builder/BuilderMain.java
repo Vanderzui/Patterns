6063375284e16738f956f15bfc2ffdc0367941f6
Vanderzui <zu.alisa@gmail.com>
2022-08-05 16:17:00 +0300

    learn/P-06: Launch main for builder

--- src/main/java/alisa/learn/java/patterns/builder/BuilderMain.java
+++ src/main/java/alisa/learn/java/patterns/builder/BuilderMain.java
@@ -1,7 +1,5 @@
 package alisa.learn.java.patterns.builder;
 
-import java.text.DecimalFormat;
-import java.text.DecimalFormatSymbols;
 import java.util.Locale;
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,24 +10,16 @@ public class BuilderMain {
     private static final Locale locale = new Locale("ru");
 
     public static void main(String[] args) {
-//        CharacterBuilder dilucBuilder = new DilucBuilder();
-//        Player player = new Player();
-//        player.setBuilder(dilucBuilder);
-//        Character diluc = player.constructCharacter();
-//        log.info(diluc.toString());
-//
-//        CharacterBuilder eulaBuilder = new EulaBuilder();
-//        player.setBuilder(eulaBuilder);
-//        Character eula = player.constructCharacter();
-//        log.info(eula.toString());
-//        log.info(BigDecimal.valueOf(1000000, 3).toPlainString().replace(".", ","));
+        CharacterBuilder dilucBuilder = new DilucBuilder();
+        Player player = new Player();
+        player.setBuilder(dilucBuilder);
+        Character diluc = player.constructCharacter();
+        log.info(diluc.toString());
 
-        DecimalFormatSymbols symbols = new DecimalFormatSymbols(Locale.getDefault());
-        symbols.setDecimalSeparator(',');
-        var decimalFormat = new DecimalFormat("#.#", symbols);
-        log.info(decimalFormat.format(10));
-        log.info(decimalFormat.format(10.0));
-        log.info(decimalFormat.format(1000.000));
-        log.info(decimalFormat.format(12345678.34));
+        CharacterBuilder eulaBuilder = new EulaBuilder();
+        player.setBuilder(eulaBuilder);
+        Character eula = player.constructCharacter();
+        log.info(eula.toString());
+
     }
 }
---
+++ src/test/java/alisa/learn/java/patterns/builder/BuilderMainTest.java
@@ -0,0 +1,13 @@
+package alisa.learn.java.patterns.builder;
+
+
+import org.junit.jupiter.api.Test;
+
+class BuilderMainTest {
+
+    @Test
+    void launchMain() {
+        final var strings = new String[]{};
+        BuilderMain.main(strings);
+    }
+}
